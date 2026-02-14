3ab706ad49d2abb60794e78a0b750e3448912ad6
Atlas Foulks <mfoulks1@gmail.com>
2026-02-14 10:33:08 -0500

    Added shadcn

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,39 +1,44 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
+import './globals.css';
+import { Sidebar } from './components/Sidebar';
 
 function Hello() {
   return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+    <div className="flex flex-col">
+      <Sidebar />
+      <div>
+        <div className="Hello">
+          <img width="200" alt="icon" src={icon} />
+        </div>
+        <h1>electron-react-boilerplate</h1>
+        <div className="Hello">
+          <a
+            href="https://electron-react-boilerplate.js.org/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button type="button">
+              <span role="img" aria-label="books">
+                📚
+              </span>
+              Read our docs
+            </button>
+          </a>
+          <a
+            href="https://github.com/sponsors/electron-react-boilerplate"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button type="button">
+              <span role="img" aria-label="folded hands">
+                🙏
+              </span>
+              Donate
+            </button>
+          </a>
+        </div>
       </div>
     </div>
   );
