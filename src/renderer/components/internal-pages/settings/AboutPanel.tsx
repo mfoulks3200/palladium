a8259c39cb02b4c512a8b9624fd0d0b4900b503c
Atlas Foulks <mfoulks1@gmail.com>
2026-02-20 18:11:05 -0500

    Fixed native deps

--- src/renderer/components/internal-pages/settings/AboutPanel.tsx
+++ src/renderer/components/internal-pages/settings/AboutPanel.tsx
@@ -1,13 +1,32 @@
 import PackageJson from '../../../../../package.json';
 
 import iconImage from '../../../../../assets/icon.png';
+import { useState } from 'react';
+import { cn } from '@/lib/utils';
 
 export const AboutPanel = () => {
+  const [iconLoaded, setIconLoaded] = useState(false);
+
+  const onLoad = () => {
+    if (!iconLoaded) {
+      setIconLoaded(true);
+    }
+  };
+
   return (
     <div className="flex h-96 w-full flex-col items-center justify-center gap-2 overflow-hidden rounded-xl">
       <img
         src={iconImage}
-        className="pointer-events-none max-h-1/2 min-h-1/2 pb-8 select-none"
+        onLoad={onLoad}
+        draggable="false"
+        className={cn(
+          'relative max-h-1/2 min-h-1/2 pb-8 opacity-0 transition-all duration-500 ease-in-out select-none',
+          'hover:scale-110 hover:rotate-3',
+          {
+            ['top-0 scale-100 opacity-100']: iconLoaded,
+            ['top-2 scale-95']: !iconLoaded,
+          },
+        )}
       />
       <div className="flex select-none">
         <div className="text-6xl font-light">Palladium</div>
